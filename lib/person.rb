faed4ade587a62778e0b5db601cd9318a2ab0863
michael otero <michaelotero1118@gmail.com>
2020-02-06 02:50:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -16,23 +16,3 @@ class Person
   end
   
 end
-
-class Person
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-
-  def job=(job)
-    @job = job
-  end 
-
-  def job 
-    @job
-  end 
-  
-end
