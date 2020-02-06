e5c81ea6068dbac9ef4993a1e44116330ad9d8ef
michael otero <michaelotero1118@gmail.com>
2020-02-06 02:48:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,17 @@ class Person
     @name = Persons_name
   end
   
+  def name
+    @name
+  end
+  
+  def job=(Persons_job)
+    @job = Persons_job
+  end
+  
   def job
     @job
   end
+  
+end
+
