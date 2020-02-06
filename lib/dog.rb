e9d13136a26dd0005c5b4f3d722b2b5cd65c7b97
michael otero <michaelotero1118@gmail.com>
2020-02-06 02:40:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,4 @@
 
-class Dog
- 
-  def initialize(first_name)
-    @first_name = fido
-  end
- 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = fido
-  end
- 
-  def name
-    "#{@first_name}".strip
-  end
- 
-end
+class Dog 
+  def name=(fido)
+    
